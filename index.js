a47827d8f020ea6435814ba871e4c42e1463624a
JeffRThomas <thomas.jeff@live.com>
2020-03-03 14:55:51 -0600

    JSLogicStart

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("JS Linked");
+
+let headerText = document.getElementById("header");
+console.log(headerText);
+
+headerText.innerText = "0";
+
+let buttonOne = document.getElementById("buttonOne");
+console.log(buttonOne);
+
+buttonOne.addEventListener("click", changes)
+function changes() {
+    buttonOne.classList.add(".differentbackground");
+    buttonOne.innerText = "1";
+}
